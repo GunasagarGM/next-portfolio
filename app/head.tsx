a692728390dd604b2787b09907da602b665b9ef7
Your Name <you@example.com>
2024-07-20 15:32:56 +0530

    header change

--- app/head.tsx
+++ app/head.tsx
@@ -2,23 +2,23 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta name="keywords" content="jigar sable, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
-      <meta property="og:site_name" content="Jigar Sable's Portfolio" />
-      <meta property="og:title" content="Portfolio | Jigar Sable - Full Stack Developer" />
+      <meta name="description" content="I'm Gunasagar, full-stack developer based in Banglore, India. " />
+      <meta name="keywords" content="Gunasagar, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="Gunasagar's Portfolio" />
+      <meta property="og:title" content="Portfolio | Gunasagar" />
       <meta property="og:description"
-        content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta property="og:url" content="https://jigarsable.vercel.app" />
+        content="I'm Jigar Sable, full-stack developer based in Banglore, India" />
+      <meta property="og:url" content="https://gunasagar.vercel.app" />
       <meta property="og:image" content="https://jigarsable.vercel.app/portfolio-fork.png" />
-      <link rel="canonical" href="https://jigarsable.vercel.app" />
+      <link rel="canonical" href="https://gunasagar.vercel.app" />
       <meta name="author" content="Jigar Sable" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://jigarsable.vercel.app" />
-      <meta name="application-name" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <meta name="apple-mobile-web-app-title" content="Portfolio | Jigar Sable - Full Stack Developer" />
+      <meta name="twitter:site" content="https://gunasagar.vercel.app" />
+      <meta name="application-name" content="Gunasagar's Portfolio" />
+      <meta name="apple-mobile-web-app-title" content="Gunasagar's Portfolio" />
       <link rel="icon" href="/favicon.ico" />
-      <title>Portfolio | Jigar Sable - Full Stack Developer</title>
+      <title>Gunasagar's Portfolio</title>
     </>
   )
 }
